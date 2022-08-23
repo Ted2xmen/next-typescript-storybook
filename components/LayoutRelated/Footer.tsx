564bf9ce354c86cc9976ad02304c63c3238de096
Ted <tugrulerdemdogru@gmail.com>
2022-08-23 16:33:28 +0300

    added active class for routes

--- components/LayoutRelated/Footer.tsx
+++ components/LayoutRelated/Footer.tsx
@@ -1,9 +1,5 @@
 const Footer = () => {
-  return (
-    <div>
-      <h4>Footer</h4>{" "}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Footer;
--- components/SubMenu.tsx
+++ components/SubMenu.tsx
@@ -1,12 +1,24 @@
-import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.ul`
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
   justify-content: center;
   list-style: none;
   padding: 20px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+  }
+
   li {
     margin-left: 36px;
     font-weight: bold;
@@ -21,6 +33,12 @@ const Wrapper = styled.ul`
       box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
         rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
     }
+  }
+
+  .active {
+    color: #eec028;
+    border: 2px solid black;
+    background-color: black;
   }
 `;
 
@@ -44,12 +62,16 @@ const SubMenuList = [
 ];
 
 const SubMenu: React.FC = () => {
+  const router = useRouter();
+
   return (
     <Wrapper>
       {SubMenuList.map((link, i) => {
         return (
           <Link key={i} href={link.path}>
-            <li className="anton">{link.name}</li>
+            <li className={router.pathname === link.path ? "active" : ""}>
+              {link.name}
+            </li>
           </Link>
         );
       })}
